7dd5e5dba871881df59ad126fe5690e5a1280381
cuonghx <cuonghx@gmail.com>
2024-10-28 15:47:59 +0700

    chore: refactor

--- src/modules/observer-accounts/dtos/create-observer-accounts.dto.ts
+++ src/modules/observer-accounts/dtos/create-observer-accounts.dto.ts
@@ -1,5 +1,5 @@
 import { IsString } from 'class-validator';
-import { IsTonAddress } from 'src/modules/common/validations/is-ton-address.class-validator';
+import { IsTonAddress } from 'src/common/validations/is-ton-address.class-validator';
 
 export class CreateObserverAccountsDto {
   @IsTonAddress()
--- src/modules/observer-accounts/dtos/set-status-observer-accounts.dto.ts
+++ src/modules/observer-accounts/dtos/set-status-observer-accounts.dto.ts
@@ -1,5 +1,5 @@
 import { IsBoolean } from 'class-validator';
-import { IsTonAddress } from 'src/modules/common/validations/is-ton-address.class-validator';
+import { IsTonAddress } from 'src/common/validations/is-ton-address.class-validator';
 
 export class SetStatusObserverAccountsParams {
   @IsTonAddress()
