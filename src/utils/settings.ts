f1e6811498a0d47a609195d141409e0706dda4b8
cuonghx <cuonghx@gmail.com>
2024-11-01 15:40:49 +0700

    Sync interval 5 secound and realtime batch size should 20

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,8 +1,8 @@
 export const getSettings = () => {
   return {
-    syncInterval: 10, // seconds
+    syncInterval: 5, // seconds
     maxRetryCount: 5,
-    realtimeTransactionsBatchSize: 10,
+    realtimeTransactionsBatchSize: 20,
     backfillTransactionsBatchSize: 100,
     backfillDelayTime: 1000, // ms
     webhookDelayTime: 1000, // ms
