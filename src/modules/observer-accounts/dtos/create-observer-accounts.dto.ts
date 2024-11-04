58ce3f6b2d27b673853c4d0211cee5fcfad22c3c
cuonghx <cuonghx@gmail.com>
2024-11-04 17:35:43 +0700

    Add API docs

--- src/modules/observer-accounts/dtos/create-observer-accounts.dto.ts
+++ src/modules/observer-accounts/dtos/create-observer-accounts.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 import { IsTonAddress } from 'src/common/validations/is-ton-address.class-validator';
 
 export class CreateObserverAccountsDto {
   @IsTonAddress()
+  @ApiProperty()
   address: string;
 
   @IsString()
+  @ApiProperty()
   name: string;
 }
--- src/modules/observer-accounts/dtos/set-status-observer-accounts.dto.ts
+++ src/modules/observer-accounts/dtos/set-status-observer-accounts.dto.ts
@@ -1,12 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean } from 'class-validator';
 import { IsTonAddress } from 'src/common/validations/is-ton-address.class-validator';
 
 export class SetStatusObserverAccountsParams {
   @IsTonAddress()
+  @ApiProperty()
   address: string;
 }
 
 export class SetStatusObserverAccountsDto {
   @IsBoolean()
+  @ApiProperty()
   stopped: boolean;
 }
