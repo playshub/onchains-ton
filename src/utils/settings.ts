5130c1123e4da1d3f22f6c408775ba07a6d96d59
cuonghx <cuonghx@gmail.com>
2024-11-06 14:28:37 +0700

    Update .env file

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,10 +1,17 @@
+import { configDotenv } from 'dotenv';
+configDotenv();
+
 export const getSettings = () => {
   return {
-    syncInterval: 5, // seconds
-    maxRetryCount: 5,
-    realtimeTransactionsBatchSize: 20,
-    backfillTransactionsBatchSize: 50,
-    backfillDelayTime: 5000, // ms
-    webhookDelayTime: 1000, // ms
+    syncInterval: Number(process.env.SYNC_INTERVAL), // seconds
+    maxRetryCount: Number(process.env.MAX_RETRY_COUNT),
+    realtimeTransactionsBatchSize: Number(
+      process.env.REALTIME_TRANSACTIONS_BATCH_SIZE,
+    ),
+    backfillTransactionsBatchSize: Number(
+      process.env.BACKFILL_TRANSACTIONS_BATCH_SIZE,
+    ),
+    backfillDelayTime: Number(process.env.BACKFILL_TRANSACTIONS_BATCH_SIZE), // ms
+    webhookDelayTime: Number(process.env.WEBHOOK_DELAY_TIME), // ms
   };
 };
